eee79ce9e172ba407abad1e5b667b52261cc268d
seop <77163842+seop-kim@users.noreply.github.com>
2025-03-06 00:07:08 +0900

    console.log2

--- index.js
+++ index.js
@@ -2,3 +2,33 @@ console.log('Hello, world!');
 console.log(1);
 console.log(true);
 console.log({name: '홍길동', age: 20, married: false});
+
+console.warn('이 코드는 잘못된 방식으로 작성된 코드입니다.');
+console.error('이 코드는 실행할 수 없습니다.');
+console.info('이 코드는 정보성 메시지입니다.');
+
+// 주석
+
+/*
+여러 줄 주석
+*/
+
+/**
+ * JSDoc 주석
+ * @param {number} a 첫 번째 숫자
+ * @param {number} b 두 번째 숫자
+ * @returns {number} 두 수의 합
+ * @example
+ * sum(1, 2) // 3
+ * sum(3, 5) // 8
+ * sum(10, -5) // 5 
+ */
+function sum(a, b) {
+    return a + b;
+} 
+
+console.log(sum(1, 2));
+console.log(sum(3, 5));
+
+
+
