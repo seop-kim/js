934250e9700b71ef0e10497cc695d2dacb7fa301
seop <77163842+seop-kim@users.noreply.github.com>
2025-03-06 23:35:34 +0900

    let, const

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log('Hello, world!');
-console.log(1);
-console.log(true);
-console.log({name: '홍길동', age: 20, married: false});
+// console.log('Hello, world!');
+// console.log(1);
+// console.log(true);
+// console.log({name: '홍길동', age: 20, married: false});
 
-console.warn('이 코드는 잘못된 방식으로 작성된 코드입니다.');
-console.error('이 코드는 실행할 수 없습니다.');
-console.info('이 코드는 정보성 메시지입니다.');
+// console.warn('이 코드는 잘못된 방식으로 작성된 코드입니다.');
+// console.error('이 코드는 실행할 수 없습니다.');
+// console.info('이 코드는 정보성 메시지입니다.');
 
 // 주석
 
@@ -14,7 +14,7 @@ console.info('이 코드는 정보성 메시지입니다.');
 */
 
 /**
- * JSDoc 주석
+ * JSDocs
  * @param {number} a 첫 번째 숫자
  * @param {number} b 두 번째 숫자
  * @returns {number} 두 수의 합
@@ -27,8 +27,15 @@ function sum(a, b) {
     return a + b;
 } 
 
-console.log(sum(1, 2));
-console.log(sum(3, 5));
+// console.log(sum(1, 2));
+// console.log(sum(3, 5));
 
 
+const SALUTATION = 'Hello, world!';
+let person = '철수';
 
+console.log(SALUTATION);
+console.log(person);
+
+let x = 1;
+console.log(x);
