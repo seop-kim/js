6f2624ed72f441fd827fbf8d80d2e5462e9e0fb0
seop <77163842+seop-kim@users.noreply.github.com>
2025-04-24 00:02:31 +0900

    feat: Add dynamic timestamp rendering to React component
    Introduce `App` and `Item` components in `ComponentEx4.js` to render and update timestamp every second. Updated `index.html` to load `ComponentEx4.js` instead of `ComponentEx3.js`. This change enhances the app with real-time dynamic data rendering.

--- TEST/react_taste/ComponentEx4.js
+++ TEST/react_taste/ComponentEx4.js
@@ -1,3 +1,53 @@
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: new Date(),
+        }
+    }
 
+    componentDidMount() {
+        this.timerId = setInterval(() => {
+            this.setState({
+                time: new Date()
+            })
+        }, 1000)
+    };
+
+    componentDidUpdate() {
+        clearInterval(this.timerId);
+    }
+
+    render() {
+        return <ul>
+            <Item value={this.state.time.toLocaleString()}/>
+            <Item value={this.state.time.toLocaleString()}/>
+            <Item value={this.state.time.toLocaleString()}/>
+        </ul>
+    }
 }
+
+class Item extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: props.value,
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.value !== this.props.value) {
+            this.setState({
+                value: this.props.value
+            })
+        }
+    }
+
+    render() {
+        return <li>Item Component {this.props.value}</li>
+    }
+}
+
+
+const container = document.getElementById('root')
+ReactDOM.render(<App/>, container)
