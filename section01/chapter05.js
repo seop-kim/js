86ca529ab060517dd1291d10c4088f56289cdc2b
seop <77163842+seop-kim@users.noreply.github.com>
2025-04-28 00:22:42 +0900

    feat: Update HTML and add data type examples to JS
    Added a meta charset to the HTML file and updated the script reference to `chapter05.js`. Enhanced `chapter05.js` with examples for various data types, including number, string, boolean, null, and undefined.

--- section01/chapter05.js
+++ section01/chapter05.js
@@ -3,3 +3,25 @@ let num1 = 27;
 let num2 = 1.5;
 let num3 = -20;
 
+let inf = Infinity;
+let negInf = -Infinity;
+let nan = NaN;
+
+
+// String type
+let myName = '김태섭';
+let myLocation = '강서';
+let introduce = `${myName}은 ${myLocation}에 거주합니다.`;
+
+
+// Boolean type
+let isSwitchOn = true;
+let isEmpty = false;
+
+
+// Null type
+let empty = null;
+
+
+// Undefined type
+let none = undefined;
