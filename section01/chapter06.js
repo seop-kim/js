a5cfd333730f7e7e659ad19b866dadb99bf28ab5
seop <77163842+seop-kim@users.noreply.github.com>
2025-04-28 23:18:56 +0900

    feat: Add explicit and implicit type conversion examples
    Updated HTML to reference a new script and added a new file, chapter06.js, demonstrating examples of explicit and implicit type conversions in JavaScript.

---
+++ section01/chapter06.js
@@ -0,0 +1,20 @@
+// 묵시적 형 변환
+let num = 10;
+let str = '20';
+
+const result = num + str;
+
+
+// 명시적 형 변환
+let str1 = '10';
+let strToNum1 = Number(str1);
+
+let str2 = '10개';
+let strToNum2 = parseInt(str2);
+
+let num1 = 20;
+let numToStr1 = String(num1);
+console.log(numToStr1 + "입니다");
+
+
+
