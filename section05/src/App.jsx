f47c8120614f08e1faa287fde5778b0bc04e9fca
seop <77163842+seop-kim@users.noreply.github.com>
2025-05-23 23:42:16 +0900

    feat: Add Header, Main, and Footer components
    Header, Main, Footer 컴포넌트를 생성하고 App.jsx에 적용하였습니다. 이를 통해 컴포넌트 기반 구조로 코드의 재사용성과 가독성을 개선하였습니다.

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import './App.css';
+// react 에서는 확장자를 작성하지 않아도 된다.
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+// Component:
+// 1. 보통 함수의 이름을 따서 만든다.
+// 2. 컴포넌트명 첫 글자는 대문자
+// 3. App component를 보통 root component 라고 한다. (App이 관례다.)
 
 function App() {
     return (
         <>
-            <h1>안녕 리액트!</h1>
+            <Header/>
+            <Main/>
+            <Footer/>
         </>
-    )
+    );
 }
 
 export default App
