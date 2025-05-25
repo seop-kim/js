443806584939919922485088868b4d4affa7588b
seop <77163842+seop-kim@users.noreply.github.com>
2025-05-25 21:50:54 +0900

    feat: Add reusable Button component
    새로운 Button 컴포넌트를 추가하여 재사용성을 높였습니다. App.jsx에 이를 활용한 다양한 버튼 예제를 구현해 기본적인 사용법을 보여줍니다.

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css';
 // react 에서는 확장자를 작성하지 않아도 된다.
 import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Button from './components/Button';
 
 // Component:
 // 1. 보통 함수의 이름을 따서 만든다.
@@ -10,13 +9,26 @@ import Footer from './components/Footer';
 // 3. App component를 보통 root component 라고 한다. (App이 관례다.)
 
 function App() {
-    return (
-        <>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </>
-    );
+
+  const buttonProps = {
+    text: '메일',
+    color: 'red',
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
+  // return 에서 컴포넌트 내부에 다른 컴포넌트나 html 등을 전달할 수 있다.
+  // 받는 컴포넌트 측에서는 children 이라는 props로 받는다.
+  return (
+      <>
+        <Button {...buttonProps}/>
+        <Button text={"카페"}/>
+        <Button text={"블로그"}>
+          <Header/>
+        </Button>
+      </>
+  );
 }
 
 export default App
---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = ({text, color = 'black', children}) => {
+  return (
+      <button style={{color: color}}>
+        {text} - {color.toUpperCase()}
+        {children}
+      </button>
+  );
+};
+
+
+export default Button;
