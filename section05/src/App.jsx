2910af7cb953a08d1ac06fb99b301df20199bd65
seop <77163842+seop-kim@users.noreply.github.com>
2025-05-26 17:48:26 +0900

    feat: Add light and count state management in App
    App 컴포넌트에 light와 count 상태 관리를 추가했습니다. 각 상태는 버튼을 사용하여 ON/OFF 또는 증가 동작을 수행하도록 구현되었습니다.

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,32 +1,28 @@
 import './App.css';
-// react 에서는 확장자를 작성하지 않아도 된다.
-import Header from './components/Header';
-import Button from './components/Button';
-
-// Component:
-// 1. 보통 함수의 이름을 따서 만든다.
-// 2. 컴포넌트명 첫 글자는 대문자
-// 3. App component를 보통 root component 라고 한다. (App이 관례다.)
+import {useState} from 'react'
 
 function App() {
-
-  const buttonProps = {
-    text: '메일',
-    color: 'red',
-    a: 1,
-    b: 2,
-    c: 3,
+  const [count, setCount] = useState(0);
+  const countButtonFunc = () => {
+    setCount(count + 1);
   }
 
-  // return 에서 컴포넌트 내부에 다른 컴포넌트나 html 등을 전달할 수 있다.
-  // 받는 컴포넌트 측에서는 children 이라는 props로 받는다.
+  const [light, setLight] = useState("OFF");
+  const lightButtonFunc = () => {
+    setLight(light === "OFF" ? "ON" : "OFF");
+  }
+
   return (
       <>
-        <Button {...buttonProps}/>
-        <Button text={"카페"}/>
-        <Button text={"블로그"}>
-          <Header/>
-        </Button>
+        <div>
+          <h1>{light}</h1>
+          <button onClick={lightButtonFunc}>{light === 'ON' ? '끄기' : '켜기'}</button>
+        </div>
+
+        <div>
+          <h1>{count}</h1>
+          <button onClick={countButtonFunc}>+</button>
+        </div>
       </>
   );
 }
