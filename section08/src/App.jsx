ac8dbf4958ac2129b379d1506d4a6943dbb0713e
seop <77163842+seop-kim@users.noreply.github.com>
2025-05-29 16:29:29 +0900

    feat: Enable adding new todos through Editor component
    Editor 컴포넌트에 새로운 todo를 추가할 수 있는 기능을 구현했습니다. 입력 필드와 버튼을 통해 todo를 생성하고, App의 상태로 관리되도록 로직을 추가했습니다.

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,16 +1,54 @@
-// CSS
-import './App.css'
+// React
+import {useRef, useState} from 'react'
 
 // Component
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List.jsx";
 
+// CSS
+import './App.css'
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    data: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    data: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    data: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      data: new Date().getTime(),
+    }
+
+    setTodos([...todos, newTodo]);
+  }
+
   return (
       <div className="App">
         <Header/>
-        <Editor/>
+        <Editor onCreate={onCreate}/>
         <List/>
       </div>
   )
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,11 +1,43 @@
+// React
+import {useRef, useState} from 'react'
+
 // CSS
 import './Editor.css'
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+  const [content, setContent] = useState('');
+  const contentRef = useRef(null);
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+
+  const onSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus();
+      return
+    }
+
+    onCreate(content);
+    setContent('');
+  }
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) { // enter
+      onSubmit();
+    }
+  }
+
   return (
       <div className='Editor'>
-        <input placeholder='새로운 Todo...'/>
-        <button>추가</button>
+        <input
+            value={content}
+            ref={contentRef}
+            placeholder='새로운 Todo...'
+            onKeyDown={onKeyDown}
+            onChange={onChangeContent}
+        />
+        <button onClick={onSubmit}>추가</button>
       </div>
   )
 }
