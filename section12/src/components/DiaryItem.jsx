0a480383e28477a5854c09b3309e81f464719161
seop <77163842+seop-kim@users.noreply.github.com>
2025-06-01 16:33:01 +0900

    feat: Add monthly filtering and sorting for diary list
    일기 리스트에 월별 필터링 및 최신순/오래된 순 정렬 기능을 추가하였습니다. Context API를 활용하여 데이터를 관리하며, 선택한 월에 따라 데이터를 필터링하고 정렬 옵션에 맞게 표시되도록 구현하였습니다.

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -1,3 +1,6 @@
+// React
+import {useNavigate} from "react-router-dom";
+
 // Component
 import Button from "./Button";
 
@@ -7,25 +10,30 @@ import {getEmotionImage} from "../util/get-emotion-image.js";
 // CSS
 import './DiaryItem.css'
 
-const DiaryItem = () => {
-  const emotionId = 5;
+const DiaryItem = ({id, emotionId, createdDate, content}) => {
+  const nav = useNavigate();
+
   return (
       <div className='DiaryItem'>
-        <div className={`img_section img_section_${emotionId}`}>
+        <div className={`img_section img_section_${emotionId}`}
+             onClick={() => nav(`/diary/${id}`)}>
           <img src={getEmotionImage(emotionId)}/>
         </div>
 
-        <div className='info_section'>
+        <div className='info_section'
+             onClick={() => nav(`/diary/${id}`)}>
           <div className='created_date'>
-            {new Date().toLocaleDateString()}
+            {new Date(createdDate).toLocaleDateString()}
           </div>
           <div className='content'>
-            일기 컨텐츠
+            {content}
           </div>
         </div>
 
-        <div className='button_section'>
-          <Button text={'수정하기'}/>
+        <div className='button_section'
+        >
+          <Button text={'수정하기'}
+                  onClick={() => nav(`/edit/${id}`)}/>
         </div>
       </div>
   )
--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,3 +1,7 @@
+// React
+import {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+
 // Component
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
@@ -5,19 +9,42 @@ import DiaryItem from "./DiaryItem";
 // CSS
 import './DiaryList.css'
 
-const DiaryList = () => {
+const DiaryList = ({data}) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState('latest');
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === 'latest') {
+        return Number(b.createdDate - a.createdDate);
+      } else {
+        return Number(a.createdDate - b.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedData();
+
   return (
       <div className='DiaryList'>
         <div className='menu_bar'>
-          <select>
+          <select onChange={onChangeSortType}>
             <option value='latest'>최신순</option>
             <option value='oldest'>오래된 순</option>
           </select>
-          <Button text='새 일기 쓰기' type={'POSITIVE'}></Button>
+          <Button text='새 일기 쓰기'
+                  type={'POSITIVE'}
+                  onClick={() => nav('/new')}></Button>
         </div>
 
         <div className='list_wrapper'>
-          <DiaryItem></DiaryItem>
+          {sortedData.map((item) => {
+            return <DiaryItem key={item.id} {...item}/>
+          })}
         </div>
       </div>
   )
