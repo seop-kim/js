0a141546a15312a9573ec9efe6e1742c1b26d061
seop <77163842+seop-kim@users.noreply.github.com>
2025-06-01 15:46:34 +0900

    feat: Add diary feature with list and item components
    Home 페이지에 DiaryList와 DiaryItem 컴포넌트를 추가하여 일기 목록 및 개별 항목 표시 기능을 구현했습니다. 각 컴포넌트는 CSS 스타일링과 기능별로 분리되었으며, App에서 필요한 CRUD 메서드를 useCallback으로 최적화했습니다.

---
+++ section12/src/components/DiaryList.jsx
@@ -0,0 +1,26 @@
+// Component
+import Button from "./Button";
+import DiaryItem from "./DiaryItem";
+
+// CSS
+import './DiaryList.css'
+
+const DiaryList = () => {
+  return (
+      <div className='DiaryList'>
+        <div className='menu_bar'>
+          <select>
+            <option value='latest'>최신순</option>
+            <option value='oldest'>오래된 순</option>
+          </select>
+          <Button text='새 일기 쓰기' type={'POSITIVE'}></Button>
+        </div>
+
+        <div className='list_wrapper'>
+          <DiaryItem></DiaryItem>
+        </div>
+      </div>
+  )
+}
+
+export default DiaryList;
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,5 +1,21 @@
+// Component
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import DiaryList from "../components/DiaryList.jsx";
+
+// CSS
+import './Home.css'
+
 const Home = () => {
-  return <div>home</div>
+  return (
+      <div>
+        <Header title='2024년 2월'
+                leftChild={<Button text={'<'}/>}
+                rightChild={<Button text={'>'}/>}
+        />
+        <DiaryList/>
+      </div>
+  );
 }
 
 export default Home;
